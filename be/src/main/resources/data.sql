5d508e461a7598538cf45b3082a4334c9f58069d
Garrett.Rathke <garrett.rathke@gmail.com>
2024-01-12 00:18:52 -0600

    updated README with containerized monorepo build and run instructions, modified seed data to conditionally insert data

--- be/src/main/resources/data.sql
+++ be/src/main/resources/data.sql
@@ -1,10 +1,12 @@
-CREATE TABLE IF NOT EXISTS house(
-    `id` bigint(20) NOT NULL AUTO_INCREMENT,
-    `name` varchar(255) DEFAULT NULL,
-    PRIMARY KEY (`id`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-DELETE FROM house;
-INSERT INTO house(name) VALUES ('Gryffindor');
-INSERT INTO house(name) VALUES ('Slytherin');
-INSERT INTO house(name) VALUES ('Hufflepuff');
-INSERT INTO house(name) VALUES ('Ravenclaw');
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = N'house')
+BEGIN
+    CREATE TABLE house(
+        `id` bigint(20) NOT NULL AUTO_INCREMENT,
+        `name` varchar(255) DEFAULT NULL,
+        PRIMARY KEY (`id`)
+    )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+    INSERT INTO house(name) VALUES ('Gryffindor');
+    INSERT INTO house(name) VALUES ('Slytherin');
+    INSERT INTO house(name) VALUES ('Hufflepuff');
+    INSERT INTO house(name) VALUES ('Ravenclaw');
+END
