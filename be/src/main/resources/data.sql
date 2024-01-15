cf4f7062d209d119a4aece5fd0973e3aa27870de
Garrett.Rathke <garrett.rathke@gmail.com>
2024-01-15 12:45:23 -0600

    added seed data

---
+++ be/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO house (name) VALUES ('Gryffindor');
+INSERT INTO house (name) VALUES ('Slytherin');
+INSERT INTO house (name) VALUES ('Ravenclaw');
+INSERT INTO house (name) VALUES ('Hufflepuff');
